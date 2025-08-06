f4bd63e4c81e3267c80a748f77d2b01787361e03
Preet2099x <preet2099x@gmail.com>
2025-08-06 16:04:02 +0530

    .

---
+++ backend/src/main/java/com/hackathon/duplicator/DuplicatorApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.duplicator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuplicatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuplicatorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hackathon/duplicator/DuplicatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.duplicator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuplicatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
